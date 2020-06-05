0961d3bab934959afd993ffdeb97835e0b5216a0
frunox <60527588+frunox@users.noreply.github.com>
2020-06-04 19:47:08 -0500

    start app refactoring

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-// import Home from "./pages/Home";
-import Banner from "./components/Banner";
-import SearchButton from "./components/SearchButton";
-import EmployeeList from "./components/EmployeeList";
-import './App.css';
+import React from "react";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+
+import "./App.css";
 
 function App() {
-  console.log('in App')
   return (
-    <div>
-      <Banner />
-      <SearchButton />
-      <EmployeeList />
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
     </div>
-    // <Router>
-    //   <div>
-    //     <Route exact path="/" component={Home} />
-    //   </div>
-    // </Router>
   );
 }
 
