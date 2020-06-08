2d811739db8ddb433e0a3061928666a634ba9445
frunox <60527588+frunox@users.noreply.github.com>
2020-06-08 10:07:00 -0500

    restyle banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -6,7 +6,7 @@ export default class Banner extends Component {
         return (
             <div className="header">
                 <h1>Employee Directory</h1>
-                <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
+                <p className="desc">Click on the column names to sort or use the search box to narrow your results.</p>
             </div>
         )
     }
